6b9ef701e7f33003ce11bada08ce7e9047f9aba6
Nathan Golba <ngolba@gmail.com>
2018-09-27 17:12:02 -0400

    fixed background

--- assets/js/app.js
+++ assets/js/app.js
@@ -6,7 +6,30 @@ $('#slider').change(() => {
     if ($('#slider').val() === "100") {
         $('#welcomeText, #slideBox').fadeOut(200)
         $('#phoneHolder').delay(200).slideUp(1000)
-        $('.centerY').delay(1200).fadeOut();
+        $('.centerY').delay(1200).hide();
         $('.afterStart').delay(1300).slideDown();
     }
+})
+
+const greeting = "Hello, my name is Nathan."
+const greeting2 = "I write code."
+
+$(document).ready(() => {
+    let i = 0;
+    let welcomeText = setInterval(() => {
+        $('#greeting').append(greeting[i])
+        i++;
+        if (i === greeting.length) {
+            clearInterval(welcomeText);
+            i = 0;
+            let secondText = setInterval(() => {
+                $('#secondText').append(greeting2[i])
+                i++;
+                if (i === greeting2.length) {
+                    clearInterval (secondText)
+                    $('#slideBox').slideDown(500);
+                }
+            }, 100)
+        }
+    }, 100)
 })
